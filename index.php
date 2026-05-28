d2c5b996a87e17efbeee1652fcc34283534abb4d
vishalparit10 <50649160+vishalparit10@users.noreply.github.com>
2026-05-28 07:44:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Cowsayphp\Farm;
 
 header('Content-Type: text/plain');
 
-$text = "Set a message by adding ?message=<message here> to the URL";
+$text = "Tushar bhai uth jaoo aur dekho thoda sikho esse ?message=<message here> to the URL";
 if(isset($_GET['message']) && $_GET['message'] != '') {
 	$text = htmlspecialchars($_GET['message']);
 }
